9d39bcdc16196c9b1c2b969725881e0aaf9015f0
seonghoonlim <hooney200@gmail.com>
2025-09-14 19:18:06 +0900

    #17 Feat: Add a click event to the main page register button

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,10 @@
+/** main.html */
+
+$(document).ready(function () {
+
+    /* register view */
+    $("#registerBtn").on("click", function () {
+        window.location.href = "users/sign-up";
+    });
+
+});
