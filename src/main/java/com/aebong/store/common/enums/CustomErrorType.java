5c20d119ef0a31207b3913ec5a98e67088a1292e
seonghoonlim <hooney200@gmail.com>
2025-09-22 23:30:33 +0900

    #29 Feat: Add custom exception and exception handlers for product register

--- src/main/java/com/aebong/store/common/enums/CustomErrorType.java
+++ src/main/java/com/aebong/store/common/enums/CustomErrorType.java
@@ -8,9 +8,18 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum CustomErrorType {
 
+    /** user domain */
     NOT_FOUND_USER          (HttpStatus.CONFLICT,              "not found user.",         "사용자를 찾을 수 없습니다."),
-    IS_EXIST_USER           (HttpStatus.CONFLICT,               "is exist user.",          "존재하는 사용자입니다."),
-    INTERNAL_SERVER_ERROR   (HttpStatus.INTERNAL_SERVER_ERROR,  "internal server error.",  "서버 내부 오류입니다.")
+    IS_EXIST_USER           (HttpStatus.CONFLICT,              "is exist user.",          "존재하는 사용자입니다."),
+
+    /** product domain */
+    IS_EXIST_PRODUCT_NAME   (HttpStatus.CONFLICT,              "is exist product name.",  "존재하는 상품명입니다."),
+    IS_EXIST_PRODUCT_CODE   (HttpStatus.CONFLICT,              "is exist product code.",  "존재하는 상품 코드입니다."),
+    INVALID_AMOUNT          (HttpStatus.CONFLICT,              "invalid amount.",         "유효하지 않은 금액입니다."),
+    INVALID_IMAGE_FORMAT    (HttpStatus.CONFLICT,              "invalid image format.",   "허용되지 않는 이미지 형식입니다."),
+
+    /** common */
+    INTERNAL_SERVER_ERROR   (HttpStatus.INTERNAL_SERVER_ERROR, "internal server error.",  "서버 내부 오류입니다.")
     ;
 
     private final HttpStatus status;
